cd78a6671604a0792bf3b5558f79457a316bf748
Giacomo Randazzo <giacomoran@gmail.com>
2024-11-22 08:08:14 +0100

    update Zellij config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -105,7 +105,7 @@ if status is-interactive
 
     if type -q zellij; and test $TERM = "alacritty"; and not set -q ZELLIJ
         # Turning this setting off allows opening new Alacritty window with a new Zellij session on [⌘ N].
-        # set ZELLIJ_AUTO_ATTACH true
+        set ZELLIJ_AUTO_ATTACH true
         # set ZELLIJ_AUTO_EXIT true
         eval (zellij setup --generate-auto-start fish | string collect)
     end
